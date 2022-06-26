f6c0bd05c7527585e9875cd0f0c42305b2b219c6
vaibhav6900 <starryvaibh786@gmail.com>
2022-06-26 15:54:51 +0530

    Updates

---
+++ precache-manifest.53099f48892478d2090182d2f91d9434.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1a30690eeab0277b3d4a25bff0b296",
+    "url": "/keep/index.html"
+  },
+  {
+    "revision": "ed64480977e0d0720b39",
+    "url": "/keep/static/js/2.06e4721e.chunk.js"
+  },
+  {
+    "revision": "9f89490c85db812b1211",
+    "url": "/keep/static/js/main.04b9b4df.chunk.js"
+  },
+  {
+    "revision": "d2b6be0ee6ba3720b76c",
+    "url": "/keep/static/js/runtime-main.faeb7f19.js"
+  }
+]);
